ea24909cd9817cd933d472669dd21f9aaead6d2d
Gaetan <gaetanfaucher1301@gmail.com>
2023-03-02 22:43:33 +0100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './components/app.component';
+import {AppRoutingModule, routingComponents} from "./app-routing.module";
+import { FooterComponent } from './components/include/footer/footer.component';
+import { HeaderComponent } from './components/include/header/header.component';
+
+@NgModule({
+  declarations: [
+    routingComponents,
+    AppComponent,
+    FooterComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
