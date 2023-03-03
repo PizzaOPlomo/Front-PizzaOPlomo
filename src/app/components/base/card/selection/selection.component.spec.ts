1876448083ca5b04c9ae14b69e0dab25c577c96f
Gaetan <gaetanfaucher1301@gmail.com>
2023-03-03 20:16:07 +0100

    Code cleanup

--- src/app/components/base/card/selection/selection.component.spec.ts
+++ src/app/components/base/card/selection/selection.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { SelectionComponent } from './selection.component';
+import {SelectionComponent} from './selection.component';
 
 describe('SelectionComponent', () => {
   let component: SelectionComponent;
@@ -8,9 +8,9 @@ describe('SelectionComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SelectionComponent ]
+      declarations: [SelectionComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
