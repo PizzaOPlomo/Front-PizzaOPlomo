1bda6611e96ca704f993433b5c24c5fa34757457
Gaetan <gaetanfaucher1301@gmail.com>
2023-03-03 21:04:46 +0100

    Update http.service.ts

--- src/app/services/http/http.service.ts
+++ src/app/services/http/http.service.ts
@@ -6,7 +6,7 @@ import {Observable} from "rxjs";
   providedIn: 'root'
 })
 export class HttpService {
-  private API_URL = 'http://localhost:3000/';
+  private API_URL: string = 'http://localhost:3000/';
 
   constructor(private http: HttpClient) {
   }
