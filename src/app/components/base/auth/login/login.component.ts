534e9c73ceaeead6387eb6a03607823dfe760fed
Gaetan <gaetanfaucher1301@gmail.com>
2023-03-03 20:44:06 +0100

    Added show password

--- src/app/components/base/auth/login/login.component.ts
+++ src/app/components/base/auth/login/login.component.ts
@@ -28,4 +28,19 @@ export class LoginComponent implements OnInit {
     alert("fields must be filled");
   }
 
+  showPassword() {
+    const passwordInput = document.getElementById("password") as HTMLInputElement;
+    const passwordIcon = document.getElementById("password-icon");
+
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+      passwordIcon?.classList.remove("fa-eye-slash");
+      passwordIcon?.classList.add("fa-eye");
+    } else {
+      passwordInput.type = "password";
+      passwordIcon?.classList.remove("fa-eye");
+      passwordIcon?.classList.add("fa-eye-slash");
+    }
+  }
+
 }
