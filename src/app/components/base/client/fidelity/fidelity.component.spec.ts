bae440af01cf16283ff8f5a67275dd7bbdcda672
Gaetan <gaetanfaucher1301@gmail.com>
2023-03-05 20:39:49 +0100

    Beginning client area

---
+++ src/app/components/base/client/fidelity/fidelity.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FidelityComponent } from './fidelity.component';
+
+describe('FidelityComponent', () => {
+  let component: FidelityComponent;
+  let fixture: ComponentFixture<FidelityComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FidelityComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FidelityComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/base/client/fidelity/fidelity.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fidelity',
+  templateUrl: './fidelity.component.html',
+  styleUrls: ['./fidelity.component.css']
+})
+export class FidelityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
